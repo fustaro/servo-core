4c778977d0552c790b9e48afc3e984ed3fb0fd30
WINDOWS-LI71O9U\Graham <graham0122@gmail.com>
2021-11-26 14:35:28 +0000

    fix MG92B direction

--- src/defaults/Servos.ts
+++ src/defaults/Servos.ts
@@ -19,7 +19,7 @@ export const TowerPro_MG92B: ServoModel = new ServoModel({
     pwmRange:   { min: 720, natural: 1500,  max: 2280 },
     angleRange: { min: -76.5, natural: 0,     max: 76.5 },
     speed: 0.08,
-    servoDirection: ServoDirection.HIGHER_PWM_CLOCKWISE
+    servoDirection: ServoDirection.LOWER_PWM_CLOCKWISE
 });
 
 export const Corona_DS339HV: ServoModel = new ServoModel({
